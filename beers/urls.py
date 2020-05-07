43883f637d8c0cbd914dc762d04eae04896971a3
Steven Cipriano <redsox333@gmail.com>
2020-05-06 23:33:03 -0700

    add basic beer view

--- beers/urls.py
+++ beers/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'beer'
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('<int:beer_id>/', views.beer_detail, name='beer_detail'),
 ]
--- beers/views.py
+++ beers/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+
+from .models import Beer
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Beers index")
+    beers = Beer.objects.order_by('name')
+    context = {'beer_list': beers}
+    return render(request, 'beers/index.html', context)
+
+def beer_detail(request, beer_id):
+    beer = get_object_or_404(Beer, pk=beer_id)
+    return render(request, 'beers/beer_detail.html', {'beer': beer})
