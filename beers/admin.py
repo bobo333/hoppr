a1436471bc521c0858d676d5af8ed6a662a8ae48
Steven Cipriano <redsox333@gmail.com>
2020-05-06 22:45:00 -0700

    initial models working

--- beers/admin.py
+++ beers/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Hops, Beer, Brewery, Review
+
 # Register your models here.
+admin.site.register(Hops)
+admin.site.register(Beer)
+admin.site.register(Brewery)
+admin.site.register(Review)
--- beers/models.py
+++ beers/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Hops(models.Model):
+    name = models.TextField('name', null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Hops"
+
+    def __str__(self):
+        return self.name
+
+class Beer(models.Model):
+    name = models.TextField('name', null=False, blank=False)
+    abv = models.IntegerField('abv', null=True, blank=True)
+    ibu = models.IntegerField('ibu', null=True, blank=True)
+    hops = models.ManyToManyField(Hops, related_name='beers', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Brewery(models.Model):
+    name = models.TextField('name', null=False, blank=False)
+    beers = models.ManyToManyField(Beer, related_name='breweries', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Breweries"
+
+    def __str__(self):
+        return self.name
+
+class Review(models.Model):
+    beer = models.ForeignKey(Beer, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField('rating', null=False, blank=False)
+    body = models.TextField('body', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
