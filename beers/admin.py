a33c38b85306c815decb2e9cc211d200e342f28b
Steven Cipriano <redsox333@gmail.com>
2020-05-07 03:03:18 -0700

    add bidirectional manytomany fields to admin for Hops and Beer

--- beers/admin.py
+++ beers/admin.py
@@ -2,8 +2,28 @@ from django.contrib import admin
 
 from .models import Hops, Beer, Brewery, Review
 
+# Hops
+class HopsBeerInline(admin.TabularInline):
+	model = Beer.hops.through
+	extra = 1
+
+class HopsAdmin(admin.ModelAdmin):
+	inlines = [
+		HopsBeerInline,
+	]
+
+# Beer
+class BeerBreweryInline(admin.TabularInline):
+	model = Brewery.beers.through
+	extra = 1
+
+class BeerAdmin(admin.ModelAdmin):
+	inlines = [
+		BeerBreweryInline,
+	]
+
 # Register your models here.
-admin.site.register(Hops)
-admin.site.register(Beer)
+admin.site.register(Hops, HopsAdmin)
+admin.site.register(Beer, BeerAdmin)
 admin.site.register(Brewery)
 admin.site.register(Review)
