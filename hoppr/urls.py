bea801740e6950ef45d4c5fc55b1bbf661354b2e
Steven Cipriano <redsox333@gmail.com>
2020-05-08 18:47:01 -0700

    use a base template

--- hoppr/urls.py
+++ hoppr/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('beers.urls')),
     path('accounts/login', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout', auth_views.LogoutView.as_view(next_page="index"), name='logout'),
+    path('accounts/logout', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
 ]
