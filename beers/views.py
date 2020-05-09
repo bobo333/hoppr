eaaf0d095037e9c83e823f9d7e1dd904b3ab3c34
Steven Cipriano <redsox333@gmail.com>
2020-05-08 21:04:57 -0700

    add basic user sign up form

--- beers/views.py
+++ beers/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import get_object_or_404, render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse
 
 from .models import Beer, Brewery, Hops
@@ -29,3 +31,22 @@ def hops_detail(request, hops_id):
 def brewery_detail(request, brewery_id):
     brewery = get_object_or_404(Brewery, pk=brewery_id)
     return render(request, 'beers/brewery_detail.html', {'brewery': brewery, 'ratings': brewery.rating_data()})
+
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect("/")
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect("/")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "registration/signup.html", {"form": form})
